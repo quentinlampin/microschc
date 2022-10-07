be7b828a6cba1b141b9195b79a21ccf7bd099db1
Quentin <quentin.lampin@orange.com>
2022-10-07 10:13:46 +0200

    IPv6 parser implementation

---
+++ microschc/parser/protocol/ipv6.py
@@ -0,0 +1,93 @@
+"""
+IPv6 header parser
+
+Parser for the IPv6 protocol header as defined in RFC8200 [1].
+
+Note 1: Hop by Hop Options, Routing header parsing is not implemented yet.
+Note 2: Fragment header parsing is not implemented as fragmentation and reassembly
+        are handled by SCHC-RF.
+Note 3: Authentication and Encapsulating Security payload parsing is not implemented yet.
+
+[1] "RFC8200: Internet Protocol, Version 6 (IPv6) Specification", S. Deering et al.
+"""
+
+from enum import Enum
+from microschc.parser import HeaderParser
+from microschc.rfc8724 import FieldDescriptor, HeaderDescriptor
+
+IPv6_HEADER_ID = 'IPv6'
+
+class IPv6Fields(str, Enum):
+    VERSION         = 'Version'
+    TRAFFIC_CLASS   = 'Traffic Class'
+    FLOW_LABEL      = 'Flow Label'
+    PAYLOAD_LENGTH  = 'Payload Length'
+    NEXT_HEADER     = 'Next Header'
+    HOP_LIMIT       = 'Hop Limit'
+    SRC_ADDRESS     = 'Source Address'
+    DST_ADDRESS     = 'Destination Address'
+
+
+class IPv6Parser(HeaderParser):
+
+    def __init__(self) -> None:
+        super().__init__(name=IPv6_HEADER_ID)
+
+    def parse(self, buffer:bytes) -> HeaderDescriptor:
+        """
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |Version| Traffic Class |           Flow Label                  |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |         Payload Length        |  Next Header  |   Hop Limit   |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                                                               |
+        +                                                               +
+        |                                                               |
+        +                         Source Address                        +
+        |                                                               |
+        +                                                               +
+        |                                                               |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                                                               |
+        +                                                               +
+        |                                                               |
+        +                      Destination Address                      +
+        |                                                               |
+        +                                                               +
+        |                                                               |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        """
+        header_bytes:bytes = buffer[0:40]
+
+        # version: 4 bits
+        version:int = (header_bytes[0] & 0xf0) >> 4
+        # traffic_class: 8 bits
+        traffic_class:int = ( (header_bytes[0] & 0x0f) << 4 ) | ( (header_bytes[1] & 0xf0) >> 4 )
+        # flow label: 20 bits
+        flow_label:int = ((header_bytes[1] & 0xf0) << 16) | (header_bytes[2] << 8) | (header_bytes[3])
+        # payload length: 16 bits
+        payload_length:int = (header_bytes[4] << 8) | (header_bytes[5])
+        # next header: 8 bits
+        next_header:int = (header_bytes[6])
+        # hop limit: 8 bits
+        hop_limit:int = ((header_bytes[7]))
+        # source address: 128 bits (16 bytes)
+        source_address:bytes = header_bytes[8:24]
+        # destination address: 128 bits (16 bytes)
+        destination_address:bytes = header_bytes[24:40]
+
+        header_descriptor:HeaderDescriptor = HeaderDescriptor(
+            id=IPv6_HEADER_ID,
+            length=40*8,
+            fields=[
+                FieldDescriptor(id=IPv6Fields.VERSION,          length=4,   position=0, value=version),
+                FieldDescriptor(id=IPv6Fields.TRAFFIC_CLASS,    length=8,   position=0, value=traffic_class),
+                FieldDescriptor(id=IPv6Fields.FLOW_LABEL,       length=20,  position=0, value=flow_label),
+                FieldDescriptor(id=IPv6Fields.PAYLOAD_LENGTH,   length=16,  position=0, value=payload_length),
+                FieldDescriptor(id=IPv6Fields.NEXT_HEADER,      length=8,   position=0, value=next_header),
+                FieldDescriptor(id=IPv6Fields.HOP_LIMIT,        length=8,   position=0, value=hop_limit),
+                FieldDescriptor(id=IPv6Fields.SRC_ADDRESS,      length=128, position=0, value=source_address),
+                FieldDescriptor(id=IPv6Fields.DST_ADDRESS,      length=128, position=0, value=destination_address)
+            ]
+        )
+        return header_descriptor
---
+++ tests/parser/protocol/test_parser_ipv6.py
@@ -0,0 +1,97 @@
+from microschc.parser.protocol.ipv6 import IPv6Parser, IPv6Fields
+from microschc.parser.parser import HeaderDescriptor
+from microschc.rfc8724 import FieldDescriptor
+
+def test_ipv6_parser_import():
+    """test: IPv6 header parser import and instanciation
+    The test instanciate an IPv6 parser and checks for import errors
+    """
+    parser = IPv6Parser()
+    assert( isinstance(parser, IPv6Parser) )
+
+def test_ipv6_parser_parse():
+    """test: IPv6 header parser parses IPv6 packet
+
+    The packet is made of an IPv6 header with following fields:
+        - id='Version'              length=4    position=0  value=6
+        - id='Traffic Class'        length=8    position=0  value=0
+        - id='Flow Label'           length=20   position=0  value=0
+        - id='Payload Length'       length=16   position=0  value=16
+        - id='Next Header'          length=8    position=0  value=17
+        - id='Hop Limit'            length=8    position=0  value=64
+        - id='Source Address'       length=128  position=0  value=b'\xfe\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa1'
+        - id='Destination Address'  length=128  position=0  value=b'\xfe\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa2'
+
+    """
+
+    valid_ipv6_packet:bytes = bytes(b"\x60\x00\x00\x00\x00\x10\x11\x40\xfe\x80\x00\x00\x00\x00\x00\x00"
+                                    b"\x00\x00\x00\x00\x00\x00\x00\xa1\xfe\x80\x00\x00\x00\x00\x00\x00"
+                                    b"\x00\x00\x00\x00\x00\x00\x00\xa2\x23\x29\x23\x2a\x00\x10\x2d\xa1"
+                                    b"\x64\x65\x61\x64\x62\x65\x65\x66"
+    )
+
+    parser:IPv6Parser = IPv6Parser()
+    ipv6_header_descriptor: HeaderDescriptor = parser.parse(buffer=valid_ipv6_packet)
+
+    # test ipv6_header_descriptor type
+    assert isinstance(ipv6_header_descriptor, HeaderDescriptor)
+
+    # test for ipv6_header_descriptor.fields length
+    assert len(ipv6_header_descriptor.fields) == 8
+
+    # test for ipv6_header_descriptor.fields types
+    for field in ipv6_header_descriptor.fields:
+        assert isinstance(field, FieldDescriptor)
+
+    # assert field descriptors match IPv6 header content
+    version_fd:FieldDescriptor = ipv6_header_descriptor.fields[0]
+    assert version_fd.id == IPv6Fields.VERSION
+    assert version_fd.length == 4
+    assert version_fd.position == 0
+    assert version_fd.value == 6
+
+    traffic_class_fd:FieldDescriptor = ipv6_header_descriptor.fields[1]
+    assert traffic_class_fd.id == IPv6Fields.TRAFFIC_CLASS
+    assert traffic_class_fd.length == 8
+    assert traffic_class_fd.position == 0
+    assert traffic_class_fd.value == 0
+
+    flow_label_fd:FieldDescriptor = ipv6_header_descriptor.fields[2]
+    assert flow_label_fd.id == IPv6Fields.FLOW_LABEL
+    assert flow_label_fd.length == 20
+    assert flow_label_fd.position == 0
+    assert flow_label_fd.value == 0
+
+    payload_length_fd:FieldDescriptor = ipv6_header_descriptor.fields[3]
+    assert payload_length_fd.id == IPv6Fields.PAYLOAD_LENGTH
+    assert payload_length_fd.length == 16
+    assert payload_length_fd.position == 0
+    assert payload_length_fd.value == 16
+
+    next_header_fd:FieldDescriptor = ipv6_header_descriptor.fields[4]
+    assert next_header_fd.id == IPv6Fields.NEXT_HEADER
+    assert next_header_fd.length == 8
+    assert next_header_fd.position == 0
+    assert next_header_fd.value == 17
+
+    hop_limit_fd:FieldDescriptor = ipv6_header_descriptor.fields[5]
+    assert hop_limit_fd.id == IPv6Fields.HOP_LIMIT
+    assert hop_limit_fd.length == 8
+    assert hop_limit_fd.position == 0
+    assert hop_limit_fd.value == 64
+
+    source_address_fd:FieldDescriptor = ipv6_header_descriptor.fields[6]
+    assert source_address_fd.id == IPv6Fields.SRC_ADDRESS
+    assert source_address_fd.length == 128
+    assert source_address_fd.position == 0
+    assert source_address_fd.value == b'\xfe\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa1'
+
+    destination_address_fd:FieldDescriptor = ipv6_header_descriptor.fields[7]
+    assert destination_address_fd.id == IPv6Fields.DST_ADDRESS
+    assert destination_address_fd.length == 128
+    assert destination_address_fd.position == 0
+    assert destination_address_fd.value == b'\xfe\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa2'
+
+    
+
+
