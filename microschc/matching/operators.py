0eb406e6e46ab358a549894beae7f113a455abff
Quentin Lampin <quentin.lampin@gmail.com>
2022-10-24 18:27:04 +0200

    MSB(X) complete rewrite: all bytes fields with left-padding

--- microschc/matching/operators.py
+++ microschc/matching/operators.py
@@ -11,11 +11,10 @@ Matching Operators (MOs) as defined in section 7.3 of RFC 8724 [1], i.e.:
 [1] "RFC 8724 SCHC: Generic Framework for Static Context Header Compression and Fragmentation" , A. Minaburo et al.
 """
 
-from typing import Any, Dict
-from microschc.rfc8724 import FieldDescriptor
+from microschc.rfc8724 import FieldDescriptor, Mapping, MatchMapping, Value
 
 
-def equal(field_descriptor: FieldDescriptor, target_value: Any) -> bool:
+def equal(field_descriptor: FieldDescriptor, target_value: Value) -> bool:
     """
     `equal` matching operator: 
     the match result is True if the field value in the packet matches the target value
@@ -33,28 +32,75 @@ def most_significant_bits(field_descriptor: FieldDescriptor, pattern: bytes, pat
     """
     `MSB(x)` matching operator:
     the match result is True if the `pattern_length` most significant (leftmost) bits of the field value equal 
-    the `pattern_length` most significant (leftmost) bits of the pattern.
+    the `pattern` of length `pattern_length`.
+
+    *Important note*: we assume that the parser provides the field value represented as bytes,
+    this means that for fields lengths differents than a multiple of 8, the byte representation
+    contains padding bits. We further assume that the field is left-padded, i.e. all padding bits
+    are on the first byte of the representation. Colloquially speaking, the raw field is packed on 
+    the right when parsed.
+
+    we also assume that the pattern is provided as bytes and that it is left-padded, if necessary.
+
+        Suppose the following field value: b'\x33\xff\x23\xdb\xda' of length 38 bits, pattern: b'\x01\x9f\xf9' of length 17 bits
+                                    field value
+            |---------------------------------------------------------------------------|
+            0x33             0xff            0x23           0xdb           0xda
+        0 0 1 1 0 0 1 1 1 1 1 1 1 1 1 1 0 0 1 0 0 0 1 1 1 1 0 1 1 0 1 1 1 1 0 1 1 0 1 0
+       |     byte0     |   byte1       |     byte2     |     byte3     |    byte4      |
+           |--------------pattern------------|-----------------residue-----------------|
+    | [...] 1|     byte1     |     byte2     |
+        0x01        0x9f            0xf9
+
+        The expected residue is:  
+                                0x3              0xdb           0xda
+                        0 0 0 0 0 0 1 1 1 1 0 1 1 0 1 1 1 1 0 1 1 0 1 0
+                        |     byte2     |     byte3     |    byte4      |
+                        |  0s |                                         | 
     """
 
-    if isinstance(field_descriptor.value, int):
-        return False
-    
-    pattern_bytes = pattern_length // 8 # number of "full" bytes in the pattern to match
-    bits_residue = pattern_length % 8 # number of bits in the pattern to match excluding "full" bytes to match
-    
-    if pattern_bytes > 0 and field_descriptor.value[0:pattern_bytes] != pattern[0:pattern_bytes]:
-        return False
-    
-    if bits_residue > 0:
-        bitmask = 0xff << (8-bits_residue) & 0xff
-        return (field_descriptor.value[pattern_bytes] & bitmask == pattern[pattern_bytes])
+    assert isinstance(field_descriptor.value, bytes)
+    field_value: bytes = field_descriptor.value
+    field_length: int = field_descriptor.length
 
-    return True
+    residue_length: int = field_length - pattern_length
+    residue_fullbytes: int = residue_length // 8
 
-def match_mapping(field_descriptor: FieldDescriptor, target_values: Dict[int, Any]) -> bool:
+    most_significant_bits: bytes = field_value[:-residue_fullbytes] # bytes 0-2
+    residue_alignment: int = residue_length % 8
+    if residue_alignment > 0:
+        left_shift: int = 8 - residue_alignment
+        region_of_interest: bytes = most_significant_bits
+        region_of_interest_length = len(region_of_interest)
+        #                  region of interest
+        #  |-------------------------------------|
+        #   0 0 1 1 0 0 1 1 1 1 1 1 1 1 1 1 0 0 1 0 0 0 1 1 
+        #  |     byte0     |   byte1       |     byte2     |
+
+        #                       pattern
+        #   0 0 0 0 0 0 0 1 1 0 0 1 1 1 1 1 1 1 1 1 1 0 0 1
+        #|      byte0      |     byte1     |     byte2     |
+        #       0x01             0x9f            0xf9
+
+
+        most_significant_bits: bytes = b''
+        bitmask: int = 0xff >> left_shift
+        for i in range(region_of_interest_length-1, 0,-1):
+            right_part: int = region_of_interest[i] >> residue_alignment
+            left_part: int =  (region_of_interest[i-1] & bitmask) << left_shift
+            byte_realigned = left_part + right_part
+            most_significant_bits = byte_realigned.to_bytes(1, 'big') + most_significant_bits
+        
+        # leading bits of region of interest
+        leading_bits = region_of_interest[0] >> residue_alignment
+        most_significant_bits = leading_bits.to_bytes(1, 'big') + most_significant_bits
+
+    return most_significant_bits == pattern
+
+def match_mapping(field_descriptor: FieldDescriptor, target_values: MatchMapping) -> bool:
     """
     `match_mapping` matching operator:
     the match result is True if the field value is in the values of the target values
     """
 
-    return (field_descriptor.value in target_values.values())
+    return (field_descriptor.value in target_values.forward.keys())
---
+++ tests/matching/test_operators.py
@@ -0,0 +1,117 @@
+from microschc.matching.operators import equal, ignore, most_significant_bits, match_mapping
+from microschc.rfc8724 import FieldDescriptor, MatchMapping
+
+
+SOME_ID = 'ID'
+
+def test_equal():
+    """test: equal matching operator
+    Test against different target values of different lengths
+    """
+    
+    # test on integer values
+    integer_target_value = 13
+    integer_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=integer_target_value)
+    assert equal(integer_field, target_value=integer_target_value) == True
+    assert equal(integer_field, target_value=integer_target_value+1) == False
+
+    # test on bytes values
+    bytes_target_value = b'\x13\xff'
+    other_bytes_value_of_same_length = b'\x14\xff'
+    same_bytes_value_of_different_length = b'\x00\x13\xff'
+    different_bytes_value_of_different_length = b'\x00\x14\xff'
+    bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=bytes_target_value)
+    assert equal(bytes_field, target_value=bytes_target_value) == True
+    assert equal(bytes_field, target_value=other_bytes_value_of_same_length) == False
+    assert equal(bytes_field, target_value=same_bytes_value_of_different_length) == False
+    assert equal(bytes_field, target_value=different_bytes_value_of_different_length) == False
+
+def test_ignore():
+    """test: ignore matching operator
+    Test that matching operator always returns True
+    """
+    any_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=0)
+    assert ignore(any_field) == True
+
+def test_most_significant_bits():
+    """test: MSB(x) matching operator
+    
+    """
+    pattern: bytes = b'\x01\x9f\xf9'
+    pattern_length = 17
+
+    # test on:
+    # - value matching the pattern
+    # - value is not byte-aligned
+    # - residue is not byte-aligned
+    # - pattern is not byte-aligned
+    pattern: bytes = b'\x01\x9f\xf9'
+    pattern_length = 17
+    field_value: bytes = b'\x33\xff\x23\xdb\xda'
+    field_length: int = 38
+
+    bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=field_length, position=0, value=field_value)
+    assert most_significant_bits(bytes_field, pattern_length=pattern_length, pattern=pattern) == True
+
+    # test on:
+    # - value matching the pattern
+    # - value is byte-aligned
+    # - pattern is not byte-aligned
+    pattern: bytes = b'\x33\xff\x23'
+    pattern_length = 24
+    field_value: bytes = b'\x33\xff\x23\xdb\xda'
+    field_length: int = 40
+
+    bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=field_length, position=0, value=field_value)
+    assert most_significant_bits(bytes_field, pattern_length=pattern_length, pattern=pattern) == True
+
+    # test on:
+    # - value not matching the pattern
+    # - value is not byte-aligned
+    # - pattern is not byte-aligned
+    pattern: bytes = b'\x01\x9f\xf9'
+    pattern_length = 17
+    field_value: bytes = b'\x34\xff\x23\xdb\xda'
+    field_length: int = 38
+
+    bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=field_length, position=0, value=field_value)
+    assert most_significant_bits(bytes_field, pattern_length=pattern_length, pattern=pattern) == False
+
+    # test on:
+    # - value not matching the pattern
+    # - value is not byte-aligned
+    # - pattern is not byte-aligned
+    pattern: bytes = b'\x01\x9f\xf9'
+    pattern_length = 17
+    field_value: bytes = b'\xf4\xff\x23\xdb\xda'
+    field_length: int = 38
+
+    bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=field_length, position=0, value=field_value)
+    assert most_significant_bits(bytes_field, pattern_length=pattern_length, pattern=pattern) == False
+
+def test_match_mapping():
+    """test: match-mapping operator
+    test the match-mapping operator on different value types
+    
+    """
+
+    integer_mapping: MatchMapping = MatchMapping(index_length=2, forward_mapping={14: 1, 21: 2, 34: 3})
+    bytes_mapping: MatchMapping = MatchMapping(index_length=2, forward_mapping={b'\xff\x13':1 , b'\xff\xff\x00':2, b'\x00':3, b'\x0e':4})
+
+    # testing on integer type fields
+    matching_integer_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=14)
+    assert match_mapping(matching_integer_field, target_values=integer_mapping) == True
+    non_matching_integer_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=15)
+    assert match_mapping(non_matching_integer_field, target_values=integer_mapping) == False
+
+    # testing on bytes fields
+    matching_bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=b'\xff\x13')
+    assert match_mapping(matching_bytes_field, target_values=bytes_mapping) == True
+    non_matching_bytes_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value=b'\xff\x15')
+    assert match_mapping(non_matching_bytes_field, target_values=bytes_mapping) == False
+
+    # testing on fields of wrong type
+    integer_field: FieldDescriptor = FieldDescriptor(id=SOME_ID, length=16, position=0, value= 14)
+    assert match_mapping(integer_field, target_values=bytes_mapping) == False
+    
+
