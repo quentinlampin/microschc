095af11809879055b0bb64b253f21626d9c6f2de
Quentin Lampin <quentin.lampin@gmail.com>
2022-10-25 17:13:24 +0200

    packet parser implementation

--- microschc/parser/parser.py
+++ microschc/parser/parser.py
@@ -1,4 +1,7 @@
-from microschc.rfc8724 import DirectionIndicator, HeaderDescriptor, PacketDescriptor
+from copy import copy
+from typing import List
+from microschc.rfc8724 import DirectionIndicator, FieldDescriptor, HeaderDescriptor, PacketDescriptor
+from microschc.rfc8724extras import ParserDefinitions
 
 
 class HeaderParser:
@@ -22,15 +25,38 @@ class PacketParser:
     A packet parser is fed bytearrays and returns a PacketDescriptor.
     It may call several header parser to parse the bytearray.
 
-    Raises:
-        NotImplementedError: This is an abstract base class. It is meant to be subclassed only.
-
     """
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, parsers: List[HeaderParser]) -> None:
         self.name = name
+        self.parsers = parsers
 
-    def parse(self, buffer: bytes, directionIndicator: DirectionIndicator) -> PacketDescriptor:
-        raise NotImplementedError
+    def parse(self, buffer: bytes, direction: DirectionIndicator) -> PacketDescriptor:
+        header_descriptors: List[HeaderDescriptor] = []
+
+        for parser in self.parsers:
+            header_descriptor = parser.parse(buffer=buffer)
+            header_descriptors.append(header_descriptor)
+
+            # update buffer to pass on to the next parser
+            bytes_consumed: int = header_descriptor.length // 8
+            extra_bits_consumed: int = header_descriptor.length % 8
+            if extra_bits_consumed != 0:
+                # TODO: circular shift on the buffer
+                pass
+            buffer = buffer[bytes_consumed:]
+        if buffer != b'':
+            payload_field:FieldDescriptor = FieldDescriptor(id=ParserDefinitions.PAYLOAD, length=len(buffer), position=0, value=buffer)
+            payload_descriptor: HeaderDescriptor = HeaderDescriptor(id=ParserDefinitions.PAYLOAD, length=len(buffer), fields=[payload_field])
+            header_descriptors.append(payload_descriptor)
+        
+        packet_descriptor: PacketDescriptor = PacketDescriptor(direction=direction, headers=header_descriptors)
+        return packet_descriptor
+
+
+
+
+
+
 
 
 
