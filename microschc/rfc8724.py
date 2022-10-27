7e4cc19cf764e86fb242e7c2576bffc72000af6a
Quentin Lampin <quentin.lampin@gmail.com>
2022-10-27 16:59:13 +0200

    wip ruler implementation (rule matching first draft)

--- microschc/rfc8724.py
+++ microschc/rfc8724.py
@@ -6,9 +6,14 @@ definitions from RFC 8724 [1] and corresponding data models.
 
 from enum import Enum
 from dataclasses import dataclass
-from typing import  Dict, List, Union
+from typing import  Dict, List, Tuple, Union
 
 Value = Union[int, bytes]
+
+@dataclass
+class Pattern:
+    pattern: bytes
+    length: int
 
 ReverseMapping = Dict[int, Value]
 Mapping = Dict[Value, int]
@@ -19,7 +24,8 @@ class MatchMapping:
         self.index_length: int = index_length
         self.forward: Mapping = forward_mapping
         self.reverse: ReverseMapping = {v: k for k, v in self.forward.items()}
-        
+
+TargetValue = Union[Value, Pattern, MatchMapping]
 
 class DirectionIndicator(str, Enum):
     UP = 'Up'
@@ -27,11 +33,17 @@ class DirectionIndicator(str, Enum):
     BIDIRECTIONAL = 'Bi'
 
 
-class MatchingOperatorID(str, Enum):
+class MatchingOperator(str, Enum):
     EQUAL = 'equal'
     IGNORE = 'ignore'
     MSB = 'MSB'
     MATCH_MAPPING = 'match-mapping'
+
+class CompressionDecompressionAction(str, Enum):
+    NOT_SENT = 'not-sent'
+    LSB = 'least-significant-bits'
+    MAPPING_SENT = 'mapping-sent'
+    VALUE_SENT = 'value-sent'
 
 
 @dataclass
@@ -59,3 +71,18 @@ class PacketDescriptor:
 class FieldResidue:
     residue: bytes
     length: int
+
+@dataclass
+class RuleFieldDescriptor:
+    id: str
+    length: int
+    position: int
+    direction: DirectionIndicator
+    target_value: TargetValue
+    matching_operator: MatchingOperator
+    compression_decompression_action: CompressionDecompressionAction
+
+@dataclass
+class RuleDescriptor:
+    id: int
+    field_descriptors: List[RuleFieldDescriptor]
---
+++ microschc/ruler/ruler.py
@@ -0,0 +1,85 @@
+"""
+The Ruler
+
+The ruler implements the logic around rules as defined in RFC 8724 [1], i.e.:
+    - rule storing: manages a collection of rules.
+    - rule matching: determine if a rule applies to a packet descriptor.
+    - packet compression: compress packets according to matching rules.
+    - packet decompression: decompress packets according to rules IDs.
+
+**Important note**: the field descriptors of a rule are supposed to be in the same order than that of targeted packets.
+The objective is that compression residues appear in the same order as corresponding headers in the source packets such that
+the order of decompressed fields at the recompression is unambiguous.
+
+[1] "RFC 8724 SCHC: Generic Framework for Static Context Header Compression and Fragmentation" , A. Minaburo et al.
+"""
+
+from copy import copy
+from typing import Dict, List
+from microschc.matching.operators import equal, ignore, match_mapping, most_significant_bits
+
+from microschc.rfc8724 import DirectionIndicator, FieldDescriptor, MatchMapping, Pattern, MatchingOperator, PacketDescriptor, RuleDescriptor, RuleFieldDescriptor, TargetValue, Value
+
+
+class Ruler:
+
+    def __init__(self, rules_descriptors: List[RuleDescriptor]) -> None:
+        default_rule: RuleDescriptor = RuleDescriptor(id=len(rules_descriptors), field_descriptors=[])
+        self.rules: List[RuleDescriptor] = rules_descriptors + [default_rule]
+
+
+    def match(self, packet_descriptor: PacketDescriptor) -> RuleDescriptor:
+        """
+        Find a rule matching the packet descriptor
+        """
+        packet_fields: List[FieldDescriptor] = []
+        
+        for header_descriptor in packet_descriptor.headers:
+            header_fields: List[FieldDescriptor] = [FieldDescriptor(id=f'{header_descriptor.id}:{f.id}', length=f.length, position=f.position, value=f.value )  for f in header_descriptor.fields]
+            packet_fields += header_fields
+
+        packet_direction: DirectionIndicator = packet_descriptor.direction
+
+        for rule in self.rules:
+            
+            # rules field descriptors and IDs that apply to packet direction
+            rule_fields: List[RuleFieldDescriptor] = [f for f in filter(lambda f: f.direction in {packet_direction, DirectionIndicator.BIDIRECTIONAL}, rule.field_descriptors)]
+            # TODO: implement alternative algorithm for mandatory/optional fields
+
+            # sanity check: both lists are of the same size
+            if len(packet_fields) != len(rule_fields):
+                break
+
+            # assert that the list of packet fields matches that of rule fields
+            if any(  _field_match(packet_field=pf, rule_field=rf) == False for (pf, rf) in zip(packet_fields, rule_fields)):
+                break
+
+            # rule matches, return it
+            return rule
+        
+
+        
+        
+
+def _field_match(packet_field: FieldDescriptor, rule_field: RuleFieldDescriptor):
+    # basic test: field IDs and length match
+    # note: with the assumption of the ordering of field descriptors in rules, the position test is unnecessary
+    if packet_field.id == rule_field.id and packet_field.length == rule_field.length:
+        return False
+    # check with the Matching Operator (MO) of the rule field
+    if rule_field.matching_operator == MatchingOperator.IGNORE:
+        return ignore(packet_field)
+
+    elif rule_field.matching_operator == MatchingOperator.EQUAL:
+        assert isinstance(rule_field.target_value, Value)
+        return equal(packet_field, rule_field.target_value)
+
+    elif rule_field.matching_operator == MatchingOperator.MSB:
+        pattern: TargetValue = rule_field.target_value
+        assert isinstance(pattern, Pattern)
+        return most_significant_bits(packet_field, pattern=pattern.pattern, pattern_length=pattern.length)
+
+    elif rule_field.matching_operator == MatchingOperator.MATCH_MAPPING:
+        mapping: TargetValue = rule_field.target_value
+        assert isinstance(mapping, MatchMapping)
+        return match_mapping(packet_field, target_values=mapping)
