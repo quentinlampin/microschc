60970534386228dbdb28ecacffe1772876cd3b69
Quentin Lampin <quentin.lampin@gmail.com>
2022-11-02 21:35:20 +0100

    rule matching test #1

--- tests/ruler/test_ruler.py
+++ tests/ruler/test_ruler.py
@@ -1,5 +1,15 @@
-from microschc.rfc8724 import CompressionDecompressionAction, DirectionIndicator, FieldDescriptor, MatchingOperator, RuleFieldDescriptor
-from microschc.ruler.ruler import _field_match
+from turtle import position
+from typing import List
+from microschc.parser.factory import factory
+from microschc.parser.parser import PacketParser
+from microschc.parser.protocol.coap import CoAPFields
+from microschc.parser.protocol.ipv6 import IPv6Fields
+from microschc.parser.protocol.udp import UDPFields
+from microschc.rfc8724 import CompressionDecompressionAction, DirectionIndicator, FieldDescriptor, HeaderDescriptor, MatchMapping, MatchingOperator, PacketDescriptor, Pattern, RuleDescriptor, RuleFieldDescriptor
+from microschc.rfc8724 import CompressionDecompressionAction as CDA
+from microschc.rfc8724 import MatchingOperator as MO
+from microschc.rfc8724extras import ParserDefinitions, StacksImplementation
+from microschc.ruler.ruler import Ruler, _field_match
 
 def test_ruler_field_match():
 
@@ -43,3 +53,83 @@ def test_ruler_field_match():
     )
     assert _field_match(packet_field=packet_field, rule_field=rule_field_equal) == False
 
+def test_rule_match():
+    valid_stack_packet:bytes = bytes(
+        b"\x60\x00\xef\x2d\x00\x68\x11\x40\x20\x01\x0d\xb8\x00\x0a\x00\x00" \
+        b"\x00\x00\x00\x00\x00\x00\x00\x02\x20\x01\x0d\xb8\x00\x0a\x00\x00" \
+        b"\x00\x00\x00\x00\x00\x00\x00\x20\xd1\x00\x16\x33\x00\x68\x5c\x21" \
+        b"\x68\x45\x22\xf6\xb8\x30\x0e\xfe\xe6\x62\x91\x22\xc1\x6e\xff\x5b" \
+        b"\x7b\x22\x62\x6e\x22\x3a\x22\x2f\x36\x2f\x22\x2c\x22\x6e\x22\x3a" \
+        b"\x22\x30\x2f\x30\x22\x2c\x22\x76\x22\x3a\x35\x34\x2e\x30\x7d\x2c" \
+        b"\x7b\x22\x6e\x22\x3a\x22\x30\x2f\x31\x22\x2c\x22\x76\x22\x3a\x34" \
+        b"\x38\x2e\x30\x7d\x2c\x7b\x22\x6e\x22\x3a\x22\x30\x2f\x35\x22\x2c" \
+        b"\x22\x76\x22\x3a\x31\x36\x36\x36\x32\x36\x33\x33\x33\x39\x7d\x5d"
+    )
+
+    packet_parser: PacketParser = factory(stack_implementation=StacksImplementation.IPV6_UDP_COAP)
+    packet_descriptor: PacketDescriptor = packet_parser.parse(buffer=valid_stack_packet, direction=DirectionIndicator.UP)
+
+    
+    field_descriptors_1: List[RuleFieldDescriptor] = [
+        RuleFieldDescriptor(
+            id=IPv6Fields.VERSION, length=4, position=0, direction=DirectionIndicator.BIDIRECTIONAL, 
+            target_value=b'\x06', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(
+            id=IPv6Fields.TRAFFIC_CLASS, length=8, position=0, direction=DirectionIndicator.BIDIRECTIONAL, 
+            target_value=b'\x00', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(
+            id=IPv6Fields.FLOW_LABEL, length=20, position=0, direction=DirectionIndicator.UP,
+            target_value=b'\x00\xef\x2d', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(
+            id=IPv6Fields.PAYLOAD_LENGTH, length=16, position=0, direction=DirectionIndicator.BIDIRECTIONAL, 
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(
+            id=IPv6Fields.NEXT_HEADER, length=8, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'\x11', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(
+            id=IPv6Fields.HOP_LIMIT, length=8, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'\x40', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(
+            id=IPv6Fields.SRC_ADDRESS, length=128, position=0, direction=DirectionIndicator.UP,
+            target_value=Pattern(pattern=b'\x20\x01\x0d\xb8\x00\x0a\x00\x00\x00\x00\x00\x00\x00\x00\x00', length=120), 
+            matching_operator=MO.MSB, compression_decompression_action=CDA.LSB),
+        RuleFieldDescriptor(id=IPv6Fields.DST_ADDRESS, length=128, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=MatchMapping(index_length=2, forward_mapping={b"\x20\x01\x0d\xb8\x00\x0a\x00\x00\x00\x00\x00\x00\x00\x00\x00\x20":0}), 
+            matching_operator=MO.MATCH_MAPPING, compression_decompression_action=CDA.MAPPING_SENT),
+
+        RuleFieldDescriptor(id=UDPFields.SOURCE_PORT, length=16, position=0, direction=DirectionIndicator.UP,
+            target_value=b'\xd1\x00', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(id=UDPFields.DESTINATION_PORT, length=16, position=0, direction=DirectionIndicator.UP,
+            target_value=b'\x16\x33', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(id=UDPFields.LENGTH, length=16, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=UDPFields.CHECKSUM, length=16, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+
+        RuleFieldDescriptor(id=CoAPFields.VERSION, length=2, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'\x01', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(id=CoAPFields.TYPE, length=2, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'\x02', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(id=CoAPFields.TOKEN_LENGTH, length=4, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=CoAPFields.CODE, length=8, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=CoAPFields.MESSAGE_ID, length=16, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=CoAPFields.TOKEN, length=64, position=0, direction=DirectionIndicator.BIDIRECTIONAL,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=CoAPFields.OPTION_DELTA, length=4, position=0, direction=DirectionIndicator.UP,
+            target_value=b'\x0c', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(id=CoAPFields.OPTION_LENGTH, length=4, position=0, direction=DirectionIndicator.UP,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=CoAPFields.OPTION_VALUE, length=0, position=0, direction=DirectionIndicator.UP,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT),
+        RuleFieldDescriptor(id=CoAPFields.PAYLOAD_MARKER, length=8, position=0, direction=DirectionIndicator.UP,
+            target_value=b'\xff', matching_operator=MO.EQUAL, compression_decompression_action=CDA.NOT_SENT),
+        RuleFieldDescriptor(id=ParserDefinitions.PAYLOAD, length=0, position=0, direction=DirectionIndicator.UP,
+            target_value=b'', matching_operator=MO.IGNORE, compression_decompression_action=CDA.VALUE_SENT)
+    ]
+    rule_descriptor_1: RuleDescriptor = RuleDescriptor(id=0, field_descriptors=field_descriptors_1)
+    ruler: Ruler = Ruler(rules_descriptors=[rule_descriptor_1])
+
+    assert ruler._match(packet_descriptor=packet_descriptor).id == rule_descriptor_1.id
