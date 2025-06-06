0d97709b6f1112fffebe95b9cc9b4058b859e4af
Quentin Lampin <quentin.lampin@gmail.com>
2025-06-06 15:34:17 +0200

    [bugfix] type check for target values for python version < 3.10

--- microschc/protocol/coap.py
+++ microschc/protocol/coap.py
@@ -643,7 +643,7 @@ def coap_option_template(
         # `option_length` is provided as a number of bytes
         if option_length is not None:
             # if option_value is not an instance of MatchMapping, Buffer, bytes, use option_length in create_target_value
-            if not isinstance(option_value, (TargetValue, bytes, list, dict)):
+            if not isinstance(option_value, (Buffer, MatchMapping, bytes, list, dict)):
                 option_value_tv: TargetValue = create_target_value(option_value, option_length*8)
             else:
                 option_value_tv: TargetValue = create_target_value(option_value)
@@ -723,7 +723,7 @@ def coap_option_template(
             option_value_fl = 0
 
         # if option_value is not an instance of MatchMapping, Buffer, bytes, use option_value_fl in create_target_value
-        if not isinstance(option_value, (TargetValue, bytes, list, dict)):
+        if not isinstance(option_value, (Buffer, MatchMapping, bytes, list, dict)):
             option_value_tv: TargetValue = create_target_value(option_value, option_value_fl)
         else:
             option_value_tv: TargetValue = create_target_value(option_value)
