a8fe0b75141381fa3f4fe10023383bcfd8dff5bc
Quentin Lampin <quentin.lampin@gmail.com>
2025-06-27 08:08:44 +0200

    [version] 0.20.6

--- microschc/__init__.py
+++ microschc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.20.5'
+__version__ = '0.20.6'
 
 
 from .rfc8724 import RuleDescriptor, RuleNature, RuleFieldDescriptor, FieldDescriptor, TargetValue, DirectionIndicator, MatchingOperator, CompressionDecompressionAction
